9f6e6a23f683bb235df893c5121c0363c416cf3d
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2024-10-26 18:12:22 +0200

    update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import os
+import uuid
 
 app = Flask(__name__)
 
@@ -7,17 +8,21 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
-@app.route('/add/{a}/{b}')
-def add():
+@app.route('/add/<a>/<b>')
+def add(a, b):
     return str(int(a)+int(b))
 
-@app.route('/add/{a}/{b}')
-def mult():
+@app.route('/mult/<a>/<b>')
+def mult(a, b):
     return str(int(a)*int(b))
 
 @app.route('/uuid')
-def mult():
-    return "02056b52-22c7-4688-946d-32ffa06eb808"
+def uuid():
+    return str(uuid.uuid4())
+
+@app.route('/cowsay/<message>')
+def uuid(message):
+    return str(uuid.uuid4() + message)
 
 if __name__ == '__main__':
     app.run(port = os.environ.get('PORT'))
